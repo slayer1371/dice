0719a0aa75e09a1e55c4ca59202e6f0f29b8da64
slayer1371 <77551419+slayer1371@users.noreply.github.com>
2021-07-30 17:59:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var n=Math.floor(Math.random()*6)+1;
 var temp="dice"+n+".png";
-var diceimg="images/"+temp;
+var diceimg=""+temp;
 document.querySelectorAll("img")[0].setAttribute("src",diceimg);
 var m=Math.floor(Math.random()*6)+1;
 var temp2="dice"+m+".png";
-var diceimg="images/"+temp2;
+var diceimg=""+temp2;
 document.querySelectorAll("img")[1].setAttribute("src",diceimg);
 
-if(n>m)
-document.querySelector("h1").innerHTML=" 🚩Player 1 Wins!";
+if(n>m){
+document.querySelector("h1").innerHTML=" 🚩Player 1 Wins!";}
 else if(n<m){
 document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
 }
